2f468c8501dc0f273d4631ab4bf91480994a0585
roya-e <rwhattever@gmail.com>
2020-01-03 22:29:01 +0330

    Revert "Changed!"
    This reverts commit 1332b58fde88f23857362f57d5f51cbc919d2cc7.

--- k1t2.c
+++ k1t2.c
@@ -2,6 +2,22 @@
 int main(){
 	int n ,y;
 	scanf("%d" ,&n);
-
+	int m=2*n -1 -n/2;
+	
+	for(int j=0 ;j<n ;j++)
+	{
+		for(int i=0 ;i< 2*n ;i++)
+		{
+			if(j> n/2)
+				y=n-1-j;
+			else
+				y=j;	
+			if(((n/2 -y)<=i && i<= (n/2+y)) || ((m -y)<=i && i<= (m+y)) )
+				printf("*");
+			else
+				printf(" ");			
+		}
+		printf("\n");
+	}
 	return 0;
 }
